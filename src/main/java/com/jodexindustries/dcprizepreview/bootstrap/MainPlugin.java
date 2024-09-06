78f3c178b68752a4ccca2ce0a6256308b67675cd
Jodexx <0984134q@gmail.com>
2024-09-06 19:23:13 +0300

    1.0.0

--- src/main/java/com/jodexindustries/dcprizepreview/bootstrap/MainPlugin.java
+++ src/main/java/com/jodexindustries/dcprizepreview/bootstrap/MainPlugin.java
@@ -1,17 +1,22 @@
-package com.jodexindustries.dcprizepreview;
+package com.jodexindustries.dcprizepreview.bootstrap;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class MainPlugin extends JavaPlugin {
+    private Loader loader;
+
+    @Override
+    public void onLoad() {
+        loader = new Loader(this);
+    }
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        loader.enable();
     }
 
     @Override
     public void onDisable() {
-        // Plugin shutdown logic
+        loader.disable();
     }
 }
--- src/main/java/com/jodexindustries/dcprizepreview/gui/EventListener.java
+++ src/main/java/com/jodexindustries/dcprizepreview/gui/EventListener.java
@@ -1,15 +1,44 @@
-package com.jodexindustries.dcprizepreview;
+package com.jodexindustries.dcprizepreview.gui;
 
+import com.jodexindustries.donatecase.api.Case;
+import com.jodexindustries.donatecase.api.data.CaseData;
 import com.jodexindustries.donatecase.api.events.CaseInteractEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.Action;
+import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.event.inventory.InventoryCloseEvent;
+import org.bukkit.inventory.Inventory;
+
+import java.util.HashSet;
+import java.util.UUID;
 
 public class EventListener implements Listener {
+    private static final HashSet<UUID> players = new HashSet<>();
+
     @EventHandler
     public void onInteract(CaseInteractEvent e) {
         if(e.getAction() == Action.LEFT_CLICK_BLOCK) {
+            CaseData caseData = Case.getCase(e.getCaseType());
+            if(caseData != null) {
+                Inventory inventory = PreviewGUI.loadGUI(caseData);
+                if(inventory == null) return;
 
+                e.getPlayer().openInventory(inventory);
+                players.add(e.getPlayer().getUniqueId());
+            }
         }
+    }
+
+    @EventHandler
+    public void onInventoryClick(InventoryClickEvent e) {
+        if(players.contains(e.getWhoClicked().getUniqueId())) {
+            e.setCancelled(true);
+        }
+    }
+
+    @EventHandler
+    public void onInventoryClose(InventoryCloseEvent e) {
+        players.remove(e.getPlayer().getUniqueId());
     }
 }
