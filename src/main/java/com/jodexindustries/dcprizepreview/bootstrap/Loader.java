706ad62601ff9693132be0da390e1ec59dff9688
Jodexx <0984134q@gmail.com>
2024-09-07 21:32:40 +0300

    Add configuration with PreviewType setting

--- src/main/java/com/jodexindustries/dcprizepreview/bootstrap/Loader.java
+++ src/main/java/com/jodexindustries/dcprizepreview/bootstrap/Loader.java
@@ -1,31 +1,36 @@
 package com.jodexindustries.dcprizepreview.bootstrap;
 
+import com.jodexindustries.dcprizepreview.config.Config;
 import com.jodexindustries.dcprizepreview.gui.EventListener;
 import com.jodexindustries.donatecase.api.addon.internal.InternalJavaAddon;
 import org.bukkit.Bukkit;
 import org.bukkit.event.HandlerList;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class Loader {
-    private JavaPlugin plugin;
+    private final Plugin plugin;
     private InternalJavaAddon addon;
     private EventListener listener;
-
-    public Loader(InternalJavaAddon addon) {
-        this.addon = addon;
-    }
 
     public Loader(JavaPlugin plugin) {
         this.plugin = plugin;
     }
 
+    public Loader(InternalJavaAddon addon) {
+        this.addon = addon;
+        this.plugin = addon.getDonateCase();
+    }
+
     public void enable() {
-        listener = new EventListener();
-        if(plugin != null) Bukkit.getServer().getPluginManager().registerEvents(listener, plugin);
-        if(addon != null) Bukkit.getServer().getPluginManager().registerEvents(listener, addon.getDonateCase());
+        Config config = addon != null ? new Config(addon) : new Config(plugin);
+        listener = new EventListener(config);
+        Bukkit.getServer().getPluginManager().registerEvents(listener, plugin);
     }
 
     public void disable() {
-        HandlerList.unregisterAll(listener);
+        if (listener != null) {
+            HandlerList.unregisterAll(listener);
+        }
     }
 }
